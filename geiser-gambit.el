3294c944d1c3b79db44ed14b133129fec454bd60
jao <jao@gnu.org>
2021-04-21 02:24:35 +0100

    Use new implementation extension declarator

--- geiser-gambit.el
+++ geiser-gambit.el
@@ -4,9 +4,9 @@
 ;; Maintainer: Daniel Leslie, Jose A Ortega Ruiz <jao@gnu.org>
 ;; Keywords: languages, gambit, scheme, geiser
 ;; Homepage: https://gitlab.com/emacs-geiser/gambit
-;; Package-Requires: ((emacs "26.1") (geiser "0.12"))
+;; Package-Requires: ((emacs "26.1") (geiser "0.16"))
 ;; SPDX-License-Identifier: BSD-3-Clause
-;; Version: 0.13
+;; Version: 0.16
 
 ;; Copyright (C) 2014, 2015, 2019, 2020, 2021 Daniel Leslie
 ;; Based on geiser-guile.el by Jose Antonio Ortega Ruiz
@@ -351,7 +351,7 @@ If t, Geiser will use `next-error' to jump to the error's location."
   (keywords geiser-gambit--keywords)
   (case-sensitive geiser-gambit-case-sensitive-p))
 
-(geiser-impl--add-to-alist 'regexp "\\.scm$" 'gambit t)
+(geiser-implementation-extension 'gambit "scm")
 
 ;;;###autoload
 (geiser-activate-implementation 'gambit)
